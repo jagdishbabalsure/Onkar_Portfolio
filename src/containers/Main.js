d86fc7ebd60b6be1a2f99522400b4a6ffe101cc3
jagdishbabalsure <jagdishbabalsure89@gmail.com>
2025-11-29 14:43:37 +0530

    Cleanup project and optimize performance

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,68 +1,79 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
 import Splash from "../pages/splash/Splash";
-import Education from "../pages/education/EducationComponent";
-import Experience from "../pages/experience/Experience";
-import Contact from "../pages/contact/ContactComponent";
-import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
-import Error404 from "../pages/errors/error404/Error";
+
+const Education = lazy(() => import("../pages/education/EducationComponent"));
+const Experience = lazy(() => import("../pages/experience/Experience"));
+const Contact = lazy(() => import("../pages/contact/ContactComponent"));
+const Projects = lazy(() => import("../pages/projects/Projects"));
+const Error404 = lazy(() => import("../pages/errors/error404/Error"));
 
 export default class Main extends Component {
   render() {
     return (
       <BrowserRouter basename="/">
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={(props) =>
-              settings.isSplash ? (
-                <Splash {...props} theme={this.props.theme} />
-              ) : (
-                <Home {...props} theme={this.props.theme} />
-              )
-            }
-          />
-          <Route
-            path="/home"
-            render={(props) => <Home {...props} theme={this.props.theme} />}
-          />
-          <Route
-            path="/experience"
-            exact
-            render={(props) => (
-              <Experience {...props} theme={this.props.theme} />
-            )}
-          />
-          <Route
-            path="/education"
-            render={(props) => (
-              <Education {...props} theme={this.props.theme} />
-            )}
-          />
-          <Route
-            path="/contact"
-            render={(props) => <Contact {...props} theme={this.props.theme} />}
-          />
-
-          {settings.isSplash && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
             <Route
-              path="/splash"
-              render={(props) => <Splash {...props} theme={this.props.theme} />}
+              path="/"
+              exact
+              render={(props) =>
+                settings.isSplash ? (
+                  <Splash {...props} theme={this.props.theme} />
+                ) : (
+                  <Home {...props} theme={this.props.theme} />
+                )
+              }
             />
-          )}
+            <Route
+              path="/home"
+              render={(props) => <Home {...props} theme={this.props.theme} />}
+            />
+            <Route
+              path="/experience"
+              exact
+              render={(props) => (
+                <Experience {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="/education"
+              render={(props) => (
+                <Education {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="/contact"
+              render={(props) => (
+                <Contact {...props} theme={this.props.theme} />
+              )}
+            />
 
-          <Route
-            path="/projects"
-            render={(props) => <Projects {...props} theme={this.props.theme} />}
-          />
-          <Route
-            path="*"
-            render={(props) => <Error404 {...props} theme={this.props.theme} />}
-          />
-        </Switch>
+            {settings.isSplash && (
+              <Route
+                path="/splash"
+                render={(props) => (
+                  <Splash {...props} theme={this.props.theme} />
+                )}
+              />
+            )}
+
+            <Route
+              path="/projects"
+              render={(props) => (
+                <Projects {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="*"
+              render={(props) => (
+                <Error404 {...props} theme={this.props.theme} />
+              )}
+            />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import "./assets/font-awesome/css/all.css";
 
 const engine = new Styletron();
@@ -21,7 +21,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
